fe5b4f0908b0c921c514dfdc84ae6ce78e274bbc
wWyo_O <zingert03@gmail.com>
2026-08-07 15:29:40 +0700

    Add ORCASHI class definition in orcashi.hpp

---
+++ orcashi.hpp
@@ -0,0 +1,41 @@
+// orcashi.hpp
+#ifndef ORCASHI_HPP
+#define ORCASHI_HPP
+
+#include "plug.hpp"
+#include <string>
+#include <atomic>
+#include <thread>
+
+class ORCASHI {
+public:
+    ORCASHI();
+    ~ORCASHI();
+    
+    bool create_room(int port = 9000);
+    bool join_room(const std::string& ip, int port = 9000);
+    bool send_message(const std::string& msg);
+    bool receive_message(std::string& msg, int timeout_ms = 100);
+    bool is_connected() const;
+    void disconnect();
+    void show_help();
+    
+    // Getters
+    std::string get_my_id() const;
+    std::string get_peer_id() const;
+    std::string get_peer_ip() const;
+    
+private:
+    TCPPlug plug;
+    std::string my_id;
+    std::atomic<bool> running;
+    std::thread ui_thread;
+    bool is_ish_mode;
+    
+    bool detect_ish();
+    std::string generate_id();
+    void ui_loop();
+    void show_banner();
+};
+
+#endif
