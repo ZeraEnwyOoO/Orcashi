a7ef1015b757a489d276ded79a1b5e7eaee527c0
wWyo_O <zingert03@gmail.com>
2026-08-07 17:14:28 +0700

    Refactor and implement Registry class functionality

--- register.cpp
+++ register.cpp
@@ -1,82 +1,172 @@
-// main.cpp - Updated with Register
-#include "orcashi.hpp"
-#include "ui.hpp"
+ // registry.cpp - IMPLEMENTATION
+#include "registry.hpp"
+#include <fstream>
 #include <iostream>
-#include <string>
+#include <cstdlib>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sstream>
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-    ORCASHI orcashi;
-    orcashi.init();
+Registry::Registry() {
+    string home = getenv("HOME");
+    registry_file = home + "/.orcashi/registry.json";
     
-    UI ui(&orcashi);
+    string dir = registry_file.substr(0, registry_file.find_last_of('/'));
+    string cmd = "mkdir -p " + dir;
+    system(cmd.c_str());
     
-    if (argc < 2) {
-        // Interactive mode
-        while (true) {
-            ui.show_main_menu();
-            char choice = ui.get_choice(0, 0);
-            
-            if (choice == 'q' || choice == 'Q') {
-                break;
-            } else if (choice == 'a' || choice == 'A') {
-                ui.show_requests();
-            } else if (choice == 'c' || choice == 'C') {
-                ui.show_peer_list();
-                int peer_choice = ui.get_choice(1, 10);
-                // Start chat with selected peer
-            } else if (choice == 'p' || choice == 'P') {
-                ui.show_peer_list();
-            } else if (choice == 'r' || choice == 'R') {
-                // Register new ID
-                orcashi.register_identity();
+    load();
+}
+
+bool Registry::register_peer(const string& id, const string& ip, const string& port) {
+    if (peers.find(id) != peers.end()) {
+        cout << "  [ERROR] ID " << id << " already registered!\n";
+        return false;
+    }
+    
+    Peer peer;
+    peer.id = id;
+    peer.ip = ip;
+    peer.port = port;
+    peer.online = true;
+    peer.last_seen = time(nullptr);
+    
+    peers[id] = peer;
+    save();
+    return true;
+}
+
+bool Registry::get_peer(const string& id, Peer& out_peer) {
+    auto it = peers.find(id);
+    if (it != peers.end()) {
+        out_peer = it->second;
+        return true;
+    }
+    return false;
+}
+
+void Registry::update_peer(const string& id, const string& ip, const string& port) {
+    auto it = peers.find(id);
+    if (it != peers.end()) {
+        it->second.ip = ip;
+        it->second.port = port;
+        it->second.last_seen = time(nullptr);
+        save();
+    }
+}
+
+void Registry::set_online(const string& id, bool online) {
+    auto it = peers.find(id);
+    if (it != peers.end()) {
+        it->second.online = online;
+        it->second.last_seen = time(nullptr);
+        save();
+    }
+}
+
+vector<Peer> Registry::get_all_peers() {
+    vector<Peer> result;
+    for (auto& pair : peers) {
+        result.push_back(pair.second);
+    }
+    return result;
+}
+
+vector<Peer> Registry::get_online_peers() {
+    vector<Peer> result;
+    for (auto& pair : peers) {
+        if (pair.second.online) {
+            result.push_back(pair.second);
+        }
+    }
+    return result;
+}
+
+bool Registry::remove_peer(const string& id) {
+    auto it = peers.find(id);
+    if (it != peers.end()) {
+        peers.erase(it);
+        save();
+        return true;
+    }
+    return false;
+}
+
+bool Registry::peer_exists(const string& id) {
+    return peers.find(id) != peers.end();
+}
+
+void Registry::load() {
+    ifstream f(registry_file.c_str());
+    if (!f.is_open()) return;
+    
+    string line;
+    Peer peer;
+    bool in_peer = false;
+    
+    while (getline(f, line)) {
+        if (line.find("\"id\":\"") != string::npos) {
+            size_t start = line.find("\"id\":\"") + 6;
+            size_t end = line.find("\"", start);
+            peer.id = line.substr(start, end - start);
+            in_peer = true;
+        }
+        
+        if (line.find("\"ip\":\"") != string::npos && in_peer) {
+            size_t start = line.find("\"ip\":\"") + 6;
+            size_t end = line.find("\"", start);
+            peer.ip = line.substr(start, end - start);
+        }
+        
+        if (line.find("\"port\":\"") != string::npos && in_peer) {
+            size_t start = line.find("\"port\":\"") + 8;
+            size_t end = line.find("\"", start);
+            peer.port = line.substr(start, end - start);
+        }
+        
+        if (line.find("\"online\":") != string::npos && in_peer) {
+            size_t start = line.find("\"online\":") + 9;
+            size_t end = line.find(",", start);
+            if (end == string::npos) end = line.find("}", start);
+            string val = line.substr(start, end - start);
+            peer.online = (val.find("true") != string::npos);
+        }
+        
+        if (line.find("}") != string::npos && in_peer) {
+            if (!peer.id.empty()) {
+                peers[peer.id] = peer;
+                peer = Peer();
+                in_peer = false;
             }
         }
-        return 0;
     }
     
-    string cmd = argv[1];
+    f.close();
+}
+
+void Registry::save() {
+    ofstream f(registry_file.c_str());
+    if (!f.is_open()) return;
     
-    if (cmd == "register") {
-        orcashi.register_identity();
-    }
-    else if (cmd == "--peer-list" || cmd == "-p") {
-        ui.show_peer_list();
-    }
-    else if (cmd == "--chat" && argc >= 3) {
-        ui.show_chat(argv[2]);
-    }
-    else if (cmd == "create") {
-        orcashi.create_room();
-    }
-    else if (cmd == "join" && argc >= 3) {
-        orcashi.join_room(argv[2]);
-    }
-    else if (cmd == "connect" && argc >= 3) {
-        orcashi.connect_peer(argv[2]);
-    }
-    else if (cmd == "peers") {
-        ui.show_peer_list();
-    }
-    else if (cmd == "--help" || cmd == "-h") {
-        cout << "\nORCASHI v3.1 - P2P Chat\n";
-        cout << "========================================\n";
-        cout << "Usage:\n";
-        cout << "  ./orcashi                - Interactive mode\n";
-        cout << "  ./orcashi register       - Register new ID\n";
-        cout << "  ./orcashi --peer-list    - Show peers\n";
-        cout << "  ./orcashi --chat <id>    - Chat with peer\n";
-        cout << "  ./orcashi create         - Create room\n";
-        cout << "  ./orcashi join <ip>      - Join by IP\n";
-        cout << "  ./orcashi connect <id>   - Connect by ID\n";
-        cout << "  ./orcashi peers          - Show peers\n";
-        cout << "  ./orcashi --help         - Show help\n";
-    }
-    else {
-        cout << "Unknown command: " << cmd << "\n";
-        cout << "Use ./orcashi --help for usage.\n";
+    f << "{\n  \"peers\": [\n";
+    
+    bool first = true;
+    for (auto& pair : peers) {
+        if (!first) f << ",\n";
+        first = false;
+        
+        Peer& p = pair.second;
+        f << "    {\n";
+        f << "      \"id\": \"" << p.id << "\",\n";
+        f << "      \"ip\": \"" << p.ip << "\",\n";
+        f << "      \"port\": \"" << p.port << "\",\n";
+        f << "      \"online\": " << (p.online ? "true" : "false") << ",\n";
+        f << "      \"last_seen\": " << p.last_seen << "\n";
+        f << "    }";
     }
     
-    return 0;
+    f << "\n  ]\n}\n";
+    f.close();
 }
