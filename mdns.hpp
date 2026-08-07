6bc4b4a98651da91bd507419d8b843f7eec5abdc
wWyo_O <zingert03@gmail.com>
2026-08-08 00:44:33 +0700

    Create mdns.hpp

---
+++ mdns.hpp
@@ -0,0 +1,23 @@
+#ifndef MDNS_HPP
+#define MDNS_HPP
+
+#include <string>
+
+class MDNS {
+public:
+    MDNS();
+    ~MDNS();
+    
+    bool init();
+    bool publish(const std::string& id, int port);
+    std::string lookup(const std::string& id);
+    void unpublish();
+    
+private:
+    std::string exec(const std::string& cmd);
+    std::string extract_ip(const std::string& output);
+    std::string service_name;
+    bool published;
+};
+
+#endif
