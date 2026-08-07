b6905b89a617890a16f47603186e96e6f50d84c7
wWyo_O <zingert03@gmail.com>
2026-08-08 01:36:20 +0700

    Update mdns.hpp

--- mdns.hpp
+++ mdns.hpp
@@ -2,6 +2,10 @@
 #define MDNS_HPP
 
 #include <string>
+#include <map>
+#include <mutex>
+#include <thread>
+#include <atomic>
 
 class MDNS {
 public:
@@ -12,12 +16,23 @@ public:
     bool publish(const std::string& id, int port);
     std::string lookup(const std::string& id);
     void unpublish();
+    bool is_running() const { return running; }
     
 private:
-    std::string exec(const std::string& cmd);
-    std::string extract_ip(const std::string& output);
-    std::string service_name;
-    bool published;
+    int sock;
+    std::string my_id;
+    std::string my_endpoint;
+    std::atomic<bool> running;
+    std::thread listen_thread;
+    std::map<std::string, std::string> cache;
+    std::mutex cache_mutex;
+    
+    void listen_loop();
+    void process_message(const std::string& msg);
+    void send_announcement(const std::string& msg);
+    void send_query(const std::string& query);
+    std::string get_local_ip();
+    void cleanup();
 };
 
 #endif
