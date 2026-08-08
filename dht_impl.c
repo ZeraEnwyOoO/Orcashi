e1de1b2e4adf767c26234d30f93028bd098b5c70
wWyo_O <zingert03@gmail.com>
2026-08-08 14:53:57 +0700

    Implement DHT functions in dht_impl.c

---
+++ dht_impl.c
@@ -0,0 +1,45 @@
+// dht_impl.c - DHT Implementation for jech/dht
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <openssl/sha.h>
+
+int dht_blacklisted(const struct sockaddr *sa, int salen) {
+    return 0;
+}
+
+int dht_random_bytes(void *buf, size_t size) {
+    FILE* f = fopen("/dev/urandom", "r");
+    if (f) {
+        size_t n = fread(buf, 1, size, f);
+        fclose(f);
+        return n == size ? 0 : -1;
+    }
+    unsigned char* b = (unsigned char*)buf;
+    for (size_t i = 0; i < size; i++) {
+        b[i] = rand() & 0xFF;
+    }
+    return 0;
+}
+
+void dht_hash(void *hash_return, int hash_size,
+              const void *data1, int len1,
+              const void *data2, int len2,
+              const void *data3, int len3) {
+    unsigned char hash[20];
+    SHA_CTX ctx;
+    SHA1_Init(&ctx);
+    SHA1_Update(&ctx, data1, len1);
+    if (data2) SHA1_Update(&ctx, data2, len2);
+    if (data3) SHA1_Update(&ctx, data3, len3);
+    SHA1_Final(hash, &ctx);
+    memcpy(hash_return, hash, hash_size > 20 ? 20 : hash_size);
+}
+
+int dht_sendto(int sockfd, const void *buf, int len, int flags,
+               const struct sockaddr *dest_addr, int addrlen) {
+    return sendto(sockfd, buf, len, flags, dest_addr, addrlen);
+}
