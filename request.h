a7bedd887dd538fa496a8c00091ec23849503fd9
wWyo_O <zingert03@gmail.com>
2026-08-09 21:27:19 +0700

    Update and rename request.hpp to request.h

---
+++ request.h
@@ -0,0 +1,39 @@
+ // request.h - Connection Requests in C
+#ifndef REQUEST_H
+#define REQUEST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+
+#define MAX_REQUESTS 1024
+
+typedef struct {
+    char from_id[64];
+    char to_id[64];
+    char status[16];  // "pending", "accepted", "rejected"
+    time_t timestamp;
+} Request;
+
+typedef struct {
+    Request requests[MAX_REQUESTS];
+    int request_count;
+    char request_file[512];
+} RequestManager;
+
+// Functions
+RequestManager* request_manager_create(void);
+void request_manager_destroy(RequestManager* rm);
+
+bool request_send(RequestManager* rm, const char* from_id, const char* to_id);
+int request_get_pending(RequestManager* rm, const char* to_id, Request* out, int max);
+bool request_accept(RequestManager* rm, const char* from_id, const char* to_id);
+bool request_reject(RequestManager* rm, const char* from_id, const char* to_id);
+bool request_exists(RequestManager* rm, const char* from_id, const char* to_id);
+
+void request_save(RequestManager* rm);
+void request_load(RequestManager* rm);
+
+#endif
