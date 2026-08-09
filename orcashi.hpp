ca555c03b0f309aa9c0c5a42b882b44ba8644582
wWyo_O <zingert03@gmail.com>
2026-08-09 16:52:48 +0700

    Replace DHTLibtorrent with DHTWrapper in orcashi.hpp

--- orcashi.hpp
+++ orcashi.hpp
@@ -2,7 +2,7 @@
 #define ORCASHI_HPP
 
 #include "plug.hpp"
-#include "dht_libtorrent.hpp"
+#include "dht_wrapper.hpp"
 #include <string>
 #include <atomic>
 #include <thread>
@@ -44,7 +44,7 @@ public:
     
 private:
     TCPPlug plug;
-    DHTLibtorrent dht;
+    DHTWrapper dht;
     std::string my_id;
     std::atomic<bool> running;
     std::thread ui_thread;
