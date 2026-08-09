a15b3447ba16f7bd14bd2ba18433e7413d5092bd
wWyo_O <zingert03@gmail.com>
2026-08-10 03:58:05 +0700

    Update ui.h

--- ui.h
+++ ui.h
@@ -25,18 +25,8 @@
 
 typedef struct {
     bool running;
-    bool glitch_enabled;
-    int glitch_intensity;
-    pthread_t glitch_thread;
     pthread_mutex_t mutex;
     pthread_mutex_t stdout_mutex;
-    bool glitch_paused;
-    
-    char* current_slogan;
-    char** slogans;
-    int slogan_count;
-    int slogan_index;
-    
     void (*on_command)(const char* cmd);
     void (*on_message)(const char* msg);
 } UI;
@@ -49,25 +39,13 @@ void ui_start(UI* ui);
 void ui_stop(UI* ui);
 
 void ui_show_banner(void);
-void ui_show_prompt(UI* ui);
 void ui_show_message(UI* ui, const char* level, const char* msg);
 void ui_show_status(UI* ui, const char* status);
 void ui_show_peer(UI* ui, const char* id, const char* ip, bool online);
 void ui_show_help(UI* ui);
 
-void ui_glitch_text(const char* text, int intensity, int delay_ms);
-void ui_type_text(const char* text, int delay_ms);
-char* ui_apply_glitch(const char* text, int intensity);
-
 char* ui_get_input(void);
 void ui_set_command_callback(UI* ui, void (*callback)(const char*));
 void ui_set_message_callback(UI* ui, void (*callback)(const char*));
-
-void ui_add_slogan(UI* ui, const char* slogan);
-void ui_set_slogans(UI* ui, const char** slogans, int count);
-void ui_next_slogan(UI* ui);
-
-int ui_random_int(int min, int max);
-char ui_random_glitch_char(void);
 
 #endif
