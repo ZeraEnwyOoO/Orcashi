ca7229713160a12a8c166db26c1f23960e071344
wWyo_O <zingert03@gmail.com>
2026-08-09 21:23:46 +0700

    Update orcashi.hpp

--- orcashi.hpp
+++ orcashi.hpp
@@ -1,60 +1,57 @@
- #ifndef ORCASHI_HPP
-#define ORCASHI_HPP
+ // orcashi.h - ORCASHI Main Header in C
+#ifndef ORCASHI_H
+#define ORCASHI_H
 
-#include "plug.hpp"
-#include "dht_wrapper.hpp"
-#include <string>
-#include <atomic>
-#include <thread>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <time.h>
+#include <ifaddrs.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <ctype.h>
 
-struct Identity {
-    std::string id;
-    std::string name;
-    std::string role;
-    std::string public_key;
-    std::string private_key_encrypted;
-    std::string signature;
-};
+#include "plug.h"
 
-class ORCASHI {
-public:
-    ORCASHI();
-    ~ORCASHI();
-    
-    bool init();
-    bool create_room(int port = 9000);
-    bool join_room(const std::string& ip, int port = 9000);
-    bool send_message(const std::string& msg);
-    bool receive_message(std::string& msg, int timeout_ms = 100);
-    bool is_connected() const;
-    void disconnect();
-    
-    std::string get_my_id() const;
-    std::string get_peer_id() const;
-    std::string get_peer_ip() const;
-    
-    bool register_identity();
-    bool connect_peer(const std::string& id);
-    bool add_peer(const std::string& id);
-    bool check_requests();
-    void show_peers();
-    void show_help();
-    
-    std::string lookup_in_dht(const std::string& id);
-    
-private:
-    TCPPlug plug;
-    DHTWrapper dht;
-    std::string my_id;
-    std::atomic<bool> running;
-    std::thread ui_thread;
-    bool is_ish_mode;
-    
-    std::string get_local_ip();
-    bool detect_ish();
-    std::string generate_id();
-    void ui_loop();
-    void show_banner();
-};
+typedef struct {
+    TCPPlug* plug;
+    char my_id[64];
+    char peer_id[64];
+    bool connected;
+    bool running;
+    char local_ip[INET_ADDRSTRLEN];
+} ORCASHI;
+
+// Main functions
+ORCASHI* orcashi_create(void);
+void orcashi_destroy(ORCASHI* orcashi);
+
+bool orcashi_init(ORCASHI* orcashi);
+bool orcashi_create_room(ORCASHI* orcashi, int port);
+bool orcashi_join_room(ORCASHI* orcashi, const char* ip, int port);
+
+bool orcashi_send_message(ORCASHI* orcashi, const char* msg);
+bool orcashi_receive_message(ORCASHI* orcashi, char* msg, int msg_size, int timeout_ms);
+bool orcashi_is_connected(ORCASHI* orcashi);
+void orcashi_disconnect(ORCASHI* orcashi);
+
+const char* orcashi_get_my_id(ORCASHI* orcashi);
+const char* orcashi_get_peer_id(ORCASHI* orcashi);
+const char* orcashi_get_peer_ip(ORCASHI* orcashi);
+
+bool orcashi_register_identity(ORCASHI* orcashi);
+bool orcashi_connect_peer(ORCASHI* orcashi, const char* id);
+
+void orcashi_show_peers(ORCASHI* orcashi);
+void orcashi_show_banner(ORCASHI* orcashi);
+void orcashi_show_help(void);
+
+// Helpers
+char* orcashi_generate_id(void);
+char* orcashi_get_local_ip(void);
 
 #endif
