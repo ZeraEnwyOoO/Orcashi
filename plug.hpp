43520c8782092eb50c8324590fe7e86b3f21360a
wWyo_O <zingert03@gmail.com>
2026-08-09 21:23:01 +0700

    Update plug.hpp
    yeah bro it dependence is c and i was stress to do with c++ so i move to c btw just translate

--- plug.hpp
+++ plug.hpp
@@ -1,46 +1,52 @@
-// plug.hpp
-#ifndef PLUG_HPP
-#define PLUG_HPP
+ // plug.h - TCP Plug in C
+#ifndef PLUG_H
+#define PLUG_H
 
-#include <string>
-#include <thread>
-#include <atomic>
-#include <queue>
-#include <mutex>
-#include <condition_variable>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <errno.h>
+#include <stdbool.h>
 
-class TCPPlug {
-public:
-    TCPPlug();
-    ~TCPPlug();
-    
-    bool create_plug(int port);
-    bool connect_to_plug(const std::string& target_ip, int port);
-    bool send_message(const std::string& msg);
-    bool receive_message(std::string& msg, int timeout_ms = 100);
-    bool is_connected() const;
-    std::string get_peer_id() const;
-    std::string get_peer_ip() const;
-    void close_connection();
-    
-private:
+typedef struct {
     int plug_socket;
     int client_socket;
-    std::atomic<bool> connected;
-    std::atomic<bool> running;
-    std::string peer_id;
-    std::string peer_ip;
+    bool connected;
+    bool running;
+    char peer_ip[INET_ADDRSTRLEN];
+    char peer_id[64];
     
-    std::thread receive_thread;
-    std::thread send_thread;
-    std::queue<std::string> message_queue;
-    std::queue<std::string> send_queue;
-    std::mutex queue_mtx;
-    std::mutex send_mtx;
-    std::condition_variable cv;
+    pthread_t receive_thread;
+    pthread_t send_thread;
+    pthread_mutex_t queue_mutex;
+    pthread_cond_t queue_cond;
     
-    void receive_loop();
-    void send_loop();
-};
+    char** message_queue;
+    char** send_queue;
+    int message_count;
+    int send_count;
+    int queue_capacity;
+} TCPPlug;
+
+// Functions
+TCPPlug* plug_create(void);
+void plug_destroy(TCPPlug* plug);
+
+bool plug_create_server(TCPPlug* plug, int port);
+bool plug_connect_client(TCPPlug* plug, const char* target_ip, int port);
+
+bool plug_send_message(TCPPlug* plug, const char* msg);
+bool plug_receive_message(TCPPlug* plug, char* msg, int msg_size, int timeout_ms);
+
+bool plug_is_connected(TCPPlug* plug);
+const char* plug_get_peer_ip(TCPPlug* plug);
+
+void plug_close_connection(TCPPlug* plug);
 
 #endif
