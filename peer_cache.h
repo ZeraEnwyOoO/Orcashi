3217fd303fad9d9f65ece030f1a9fa5375bc7c3f
wWyo_O <zingert03@gmail.com>
2026-08-11 04:18:59 +0700

    Update peer_cache.h

--- peer_cache.h
+++ peer_cache.h
@@ -1,5 +1,4 @@
- // peer_cache.h - Peer Cache in C
-#ifndef PEER_CACHE_H
+ #ifndef PEER_CACHE_H
 #define PEER_CACHE_H
 
 #include <stdio.h>
@@ -7,41 +6,46 @@
 #include <string.h>
 #include <stdbool.h>
 #include <time.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 
 #define MAX_CACHE_PEERS 1024
-#define PEER_CACHE_FILE "/tmp/.orcashi/peers.json"
+#define CACHE_FILE "/tmp/.orcashi/peers.cache"
 
 typedef struct {
     char id[64];
+    char endpoint[128];
     char ip[INET_ADDRSTRLEN];
     int port;
-    char endpoint[128];
-    bool online;
+    char name[128];
     time_t last_seen;
+    bool online;
+    char public_key[512];
 } CachePeer;
 
 typedef struct {
     CachePeer peers[MAX_CACHE_PEERS];
     int peer_count;
     char cache_file[512];
-    bool modified;
     pthread_mutex_t mutex;
+    bool dirty;
 } PeerCache;
 
-// Core functions
 PeerCache* peer_cache_create(void);
 void peer_cache_destroy(PeerCache* pc);
 
-bool peer_cache_save_peer(PeerCache* pc, const CachePeer* peer);
+void peer_cache_save_peer(PeerCache* pc, const CachePeer* peer);
 bool peer_cache_get_peer(PeerCache* pc, const char* id, CachePeer* out_peer);
 int peer_cache_get_all(PeerCache* pc, CachePeer* peers, int max_peers);
+void peer_cache_remove_peer(PeerCache* pc, const char* id);
 void peer_cache_clear(PeerCache* pc);
 
-void peer_cache_auto_save(PeerCache* pc);
-
-// Load/Save
 void peer_cache_load(PeerCache* pc);
 void peer_cache_save(PeerCache* pc);
+void peer_cache_auto_save(PeerCache* pc);
 
 #endif
