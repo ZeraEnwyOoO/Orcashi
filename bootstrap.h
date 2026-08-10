2dfd9394cc2569547794dabd38c9170b9055bd2c
wWyo_O <zingert03@gmail.com>
2026-08-10 18:55:12 +0700

    Update bootstrap.h

--- bootstrap.h
+++ bootstrap.h
@@ -1,15 +1,17 @@
-#ifndef BOOTSTRAP_H
+ #ifndef BOOTSTRAP_H
 #define BOOTSTRAP_H
 
-#define BOOTSTRAP_NODES 4
+#define BOOTSTRAP_NODES 6
 
 typedef struct {
     char host[64];
     int port;
+    char ip[INET_ADDRSTRLEN];
 } BootstrapNode;
 
 extern BootstrapNode bootstrap_nodes[BOOTSTRAP_NODES];
 
 void bootstrap_init(void);
+int bootstrap_get_node(int index, BootstrapNode* node);
 
 #endif
