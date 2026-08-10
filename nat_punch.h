c0fd2e3ccf0e81aa4080154c1a4e30e3564e0228
wWyo_O <zingert03@gmail.com>
2026-08-10 18:54:36 +0700

    Update nat_punch.h

--- nat_punch.h
+++ nat_punch.h
@@ -1,8 +1,9 @@
-#ifndef NAT_PUNCH_H
+ #ifndef NAT_PUNCH_H
 #define NAT_PUNCH_H
 
 #include <stdbool.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define PUNCH_PORT 33445
 #define MAX_RETRY 10
@@ -12,11 +13,13 @@ typedef struct {
     int local_port;
     char peer_ip[INET_ADDRSTRLEN];
     int peer_port;
+    bool punched;
 } PunchState;
 
 int punch_init(PunchState* p, int port);
 int punch_send(PunchState* p, const char* target_ip, int target_port);
 int punch_listen(PunchState* p, char* peer_ip, int* peer_port);
+int punch_punch(PunchState* p, const char* target_ip, int target_port);
 void punch_close(PunchState* p);
 
 #endif
