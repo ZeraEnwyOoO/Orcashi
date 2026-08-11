b1677e261eceb7628dc4fd9d41e5cf2331eda791
wWyo_O <zingert03@gmail.com>
2026-08-11 09:35:51 +0700

    Update plug.h

--- plug.h
+++ plug.h
@@ -20,12 +20,10 @@ typedef struct {
     bool running;
     char peer_ip[INET_ADDRSTRLEN];
     char peer_id[64];
-    
     pthread_t receive_thread;
     pthread_t send_thread;
     pthread_mutex_t queue_mutex;
     pthread_cond_t queue_cond;
-    
     char** message_queue;
     char** send_queue;
     int message_count;
@@ -35,17 +33,13 @@ typedef struct {
 
 TCPPlug* plug_create(void);
 void plug_destroy(TCPPlug* plug);
-
 bool plug_create_server(TCPPlug* plug, int port);
 bool plug_connect_client(TCPPlug* plug, const char* target_ip, int port);
-
 bool plug_send_message(TCPPlug* plug, const char* msg);
 bool plug_receive_message(TCPPlug* plug, char* msg, int msg_size, int timeout_ms);
-
 bool plug_is_connected(TCPPlug* plug);
 const char* plug_get_peer_ip(TCPPlug* plug);
 int plug_get_socket(TCPPlug* plug);
-
 void plug_close_connection(TCPPlug* plug);
 
 #endif
