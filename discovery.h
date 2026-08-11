c429941ec3c96acb44fd18038aae068a65b121fc
wWyo_O <zingert03@gmail.com>
2026-08-11 09:36:52 +0700

    Update discovery.h

--- discovery.h
+++ discovery.h
@@ -1,5 +1,4 @@
- cat > discovery.h << 'EOF'
-#ifndef DISCOVERY_H
+ #ifndef DISCOVERY_H
 #define DISCOVERY_H
 
 #include <stdio.h>
@@ -35,40 +34,28 @@ typedef struct {
     int udp_socket;
     int port;
     bool running;
-    
     pthread_t listen_thread;
     pthread_t broadcast_thread;
     pthread_mutex_t mutex;
-    
     PeerInfo peers[MAX_PEERS];
     int peer_count;
-    
     void (*on_peer_found)(PeerInfo* peer);
     void (*on_peer_offline)(PeerInfo* peer);
 } Discovery;
 
 Discovery* discovery_create(void);
 void discovery_destroy(Discovery* disc);
-
 bool discovery_init(Discovery* disc, int port);
 void discovery_start(Discovery* disc);
 void discovery_stop(Discovery* disc);
-
 void discovery_broadcast_presence(Discovery* disc, const char* id, const char* endpoint);
 void discovery_broadcast_search(Discovery* disc, const char* id);
-
 bool discovery_find_peer(Discovery* disc, const char* id, PeerInfo* out_peer);
 int discovery_get_peers(Discovery* disc, PeerInfo* peers, int max_peers);
-
 void discovery_cleanup_stale(Discovery* disc);
-
 char* discovery_get_local_ip(void);
-
 void discovery_set_on_peer_found(Discovery* disc, void (*callback)(PeerInfo*));
 void discovery_set_on_peer_offline(Discovery* disc, void (*callback)(PeerInfo*));
-
-// NEW: Set identity for search responses
 void discovery_set_my_identity(Discovery* disc, const char* id, const char* ip, int port);
 
 #endif
-EOF
