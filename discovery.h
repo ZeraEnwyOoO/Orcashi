95d050993bdc96dbf156b944e943e1ea57c14507
wWyo_O <zingert03@gmail.com>
2026-08-12 21:57:29 +0700

    Update discovery.h

--- discovery.h
+++ discovery.h
@@ -17,7 +17,7 @@
 
 #define DISCOVERY_PORT 9001
 #define MAX_PEERS 256
-#define PEER_TIMEOUT 300  // 5 minutes
+#define PEER_TIMEOUT 300
 
 typedef struct {
     char id[64];
@@ -31,6 +31,12 @@ typedef struct {
 } PeerInfo;
 
 typedef struct {
+    char from_id[64];
+    char from_ip[INET_ADDRSTRLEN];
+    int from_port;
+} PendingRequest;
+
+typedef struct {
     int udp_socket;
     int port;
     bool running;
@@ -39,40 +45,32 @@ typedef struct {
     pthread_mutex_t mutex;
     PeerInfo peers[MAX_PEERS];
     int peer_count;
+    PendingRequest pending_requests[MAX_PEERS];
+    int pending_count;
     void (*on_peer_found)(PeerInfo* peer);
     void (*on_peer_offline)(PeerInfo* peer);
 } Discovery;
 
-// ===== Create / Destroy =====
 Discovery* discovery_create(void);
 void discovery_destroy(Discovery* disc);
-
-// ===== Init / Start / Stop =====
 bool discovery_init(Discovery* disc, int port);
 void discovery_start(Discovery* disc);
 void discovery_stop(Discovery* disc);
-
-// ===== Broadcast =====
 void discovery_broadcast_presence(Discovery* disc, const char* id, const char* endpoint);
 void discovery_broadcast_search(Discovery* disc, const char* id);
-
-// ===== Active Query =====
 void discovery_query_peer(Discovery* disc, const char* id);
-
-// ===== Friend Request (Reliable UDP with ACK) =====
 void discovery_send_add_request_with_ack(Discovery* disc, const char* target_id, const char* my_id, const char* my_ip, int my_port);
 void discovery_send_add_request_ack(Discovery* disc, const char* target_id, const char* from_id);
-
-// ===== Peer Management =====
 bool discovery_find_peer(Discovery* disc, const char* id, PeerInfo* out_peer);
 int discovery_get_peers(Discovery* disc, PeerInfo* peers, int max_peers);
 void discovery_cleanup_stale(Discovery* disc);
-
-// ===== Identity =====
 void discovery_set_my_identity(Discovery* disc, const char* id, const char* ip, int port);
+void discovery_set_request_manager(RequestManager* rm);
+void discovery_set_registry(Registry* reg);
+void discovery_push_pending(Discovery* disc, const char* from_id, const char* from_ip, int from_port);
+bool discovery_pop_pending(Discovery* disc, PendingRequest* out);
+int discovery_pending_count(Discovery* disc);
 char* discovery_get_local_ip(void);
-
-// ===== Callbacks =====
 void discovery_set_on_peer_found(Discovery* disc, void (*callback)(PeerInfo*));
 void discovery_set_on_peer_offline(Discovery* disc, void (*callback)(PeerInfo*));
 
