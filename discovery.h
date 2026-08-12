6089f525b455388de4c1f7d270647ccb5f0d5c27
wWyo_O <zingert03@gmail.com>
2026-08-12 13:33:16 +0700

    Update discovery.h

--- discovery.h
+++ discovery.h
@@ -43,22 +43,36 @@ typedef struct {
     void (*on_peer_offline)(PeerInfo* peer);
 } Discovery;
 
+// ===== Create / Destroy =====
 Discovery* discovery_create(void);
 void discovery_destroy(Discovery* disc);
+
+// ===== Init / Start / Stop =====
 bool discovery_init(Discovery* disc, int port);
 void discovery_start(Discovery* disc);
 void discovery_stop(Discovery* disc);
+
+// ===== Broadcast =====
 void discovery_broadcast_presence(Discovery* disc, const char* id, const char* endpoint);
 void discovery_broadcast_search(Discovery* disc, const char* id);
+
+// ===== Active Query =====
+void discovery_query_peer(Discovery* disc, const char* id);
+
+// ===== Friend Request =====
+void discovery_send_add_request(Discovery* disc, const char* target_id, const char* my_id, const char* my_ip, int my_port);
+
+// ===== Peer Management =====
 bool discovery_find_peer(Discovery* disc, const char* id, PeerInfo* out_peer);
 int discovery_get_peers(Discovery* disc, PeerInfo* peers, int max_peers);
 void discovery_cleanup_stale(Discovery* disc);
+
+// ===== Identity =====
+void discovery_set_my_identity(Discovery* disc, const char* id, const char* ip, int port);
 char* discovery_get_local_ip(void);
+
+// ===== Callbacks =====
 void discovery_set_on_peer_found(Discovery* disc, void (*callback)(PeerInfo*));
 void discovery_set_on_peer_offline(Discovery* disc, void (*callback)(PeerInfo*));
-void discovery_set_my_identity(Discovery* disc, const char* id, const char* ip, int port);
-
-// ===== NEW: Active Query =====
-void discovery_query_peer(Discovery* disc, const char* id);
 
 #endif
