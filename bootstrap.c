2777e073688dd133df100071da2c87becd349145
wWyo_O <zingert03@gmail.com>
2026-08-14 09:17:04 +0700

    Update bootstrap.c

--- bootstrap.c
+++ bootstrap.c
@@ -2,9 +2,10 @@
 #include "dht.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <netdb.h>
 #include <arpa/inet.h>
-#include <stdlib.h>
+#include <sys/socket.h>
 
 BootstrapNode bootstrap_nodes[BOOTSTRAP_NODES] = {
     {"router.bittorrent.com", 6881, ""},
@@ -24,15 +25,37 @@ static const char* fallback_ips[BOOTSTRAP_NODES] = {
     "144.217.249.33"
 };
 
+/* Resolve hostname using getaddrinfo (works on musl) */
+static int resolve_host(const char* host, char* ipbuf, size_t bufsize) {
+    struct addrinfo hints, *res, *rp;
+    int status;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;      /* IPv4 only for now */
+    hints.ai_socktype = SOCK_STREAM;
+
+    status = getaddrinfo(host, NULL, &hints, &res);
+    if (status != 0) {
+        return -1;
+    }
+
+    for (rp = res; rp != NULL; rp = rp->ai_next) {
+        struct sockaddr_in* addr = (struct sockaddr_in*)rp->ai_addr;
+        void* sin_addr = &addr->sin_addr;
+        if (inet_ntop(AF_INET, sin_addr, ipbuf, bufsize) != NULL) {
+            freeaddrinfo(res);
+            return 0;
+        }
+    }
+    freeaddrinfo(res);
+    return -1;
+}
+
 void bootstrap_init(void) {
     printf("[BOOTSTRAP] Loading %d bootstrap nodes...\n", BOOTSTRAP_NODES);
     
     for (int i = 0; i < BOOTSTRAP_NODES; i++) {
-        struct hostent* he = gethostbyname(bootstrap_nodes[i].host);
-        if (he && he->h_addr_list[0]) {
-            struct sockaddr_in addr;
-            memcpy(&addr.sin_addr, he->h_addr_list[0], he->h_length);
-            inet_ntop(AF_INET, &addr.sin_addr, bootstrap_nodes[i].ip, INET_ADDRSTRLEN);
+        if (resolve_host(bootstrap_nodes[i].host, bootstrap_nodes[i].ip, INET_ADDRSTRLEN) == 0) {
             printf("[BOOTSTRAP] %s -> %s\n", bootstrap_nodes[i].host, bootstrap_nodes[i].ip);
         } else {
             printf("[BOOTSTRAP] DNS failed for %s, using fallback\n", bootstrap_nodes[i].host);
