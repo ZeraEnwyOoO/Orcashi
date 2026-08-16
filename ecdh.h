2e74d59f64e856c0ed9bd0010ccc7d29a365260e
wWyo_O <zingert03@gmail.com>
2026-08-16 13:49:26 +0700

    Update ecdh.h

--- ecdh.h
+++ ecdh.h
@@ -10,18 +10,10 @@
 extern "C" {
 #endif
 
-/* ============================================================================
- * CONSTANTS
- * ============================================================================ */
-
 #define ORCA_ECDH_PUBLIC_KEY_LEN 32
 #define ORCA_ECDH_PRIVATE_KEY_LEN 32
 #define ORCA_ECDH_SHARED_SECRET_LEN 32
 #define ORCA_ECDH_KEY_AGREEMENT_LEN 32
-
-/* ============================================================================
- * DATA TYPES
- * ============================================================================ */
 
 typedef struct {
     unsigned char public_key[ORCA_ECDH_PUBLIC_KEY_LEN];
@@ -32,20 +24,12 @@ typedef struct {
     unsigned char shared_secret[ORCA_ECDH_SHARED_SECRET_LEN];
 } OrcaECDHSharedSecret;
 
-/* ============================================================================
- * KEYPAIR MANAGEMENT
- * ============================================================================ */
-
 int orca_ecdh_generate_keypair(OrcaECDHKeypair* keypair_out);
 int orca_ecdh_generate_ephemeral(OrcaECDHKeypair* keypair_out);
 char* orca_ecdh_public_key_to_hex(const unsigned char* public_key, char* hex_out);
 int orca_ecdh_hex_to_public_key(const char* hex, unsigned char* public_key_out);
 char* orca_ecdh_private_key_to_hex(const unsigned char* private_key, char* hex_out);
 int orca_ecdh_hex_to_private_key(const char* hex, unsigned char* private_key_out);
-
-/* ============================================================================
- * KEY EXCHANGE
- * ============================================================================ */
 
 int orca_ecdh_compute_shared_secret(const unsigned char* private_key,
                                     const unsigned char* peer_public_key,
@@ -56,10 +40,6 @@ int orca_ecdh_compute_shared_secret_keypair(const OrcaECDHKeypair* keypair,
 int orca_ecdh_compute_shared_secret_hex(const char* private_key_hex,
                                         const char* peer_public_key_hex,
                                         char* shared_secret_hex_out);
-
-/* ============================================================================
- * KEY DERIVATION (HKDF)
- * ============================================================================ */
 
 int orca_ecdh_derive_aes_key(const unsigned char* shared_secret,
                              const unsigned char* salt, size_t salt_len,
@@ -73,10 +53,6 @@ int orca_ecdh_derive_keys(const unsigned char* shared_secret,
                           const unsigned char* salt, size_t salt_len,
                           int key_count,
                           unsigned char* keys_out, size_t key_len);
-
-/* ============================================================================
- * SESSION MANAGEMENT
- * ============================================================================ */
 
 typedef struct {
     unsigned char session_id[32];
@@ -105,20 +81,12 @@ void orca_ecdh_session_destroy(OrcaECDSession* session);
 bool orca_ecdh_session_is_established(OrcaECDSession* session);
 bool orca_ecdh_session_expired(OrcaECDSession* session);
 
-/* ============================================================================
- * KEY SERIALIZATION
- * ============================================================================ */
-
 int orca_ecdh_keypair_to_hex(const OrcaECDHKeypair* keypair,
                              char* public_hex_out,
                              char* private_hex_out);
 int orca_ecdh_keypair_from_hex(const char* public_hex,
                                const char* private_hex,
                                OrcaECDHKeypair* keypair_out);
-
-/* ============================================================================
- * PERFECT FORWARD SECRECY
- * ============================================================================ */
 
 int orca_ecdh_generate_pfs_keypair(OrcaECDHKeypair* keypair_out,
                                    const unsigned char* seed,
@@ -128,20 +96,12 @@ int orca_ecdh_compute_pfs_secret(const unsigned char* private_key,
                                  const unsigned char* previous_secret,
                                  unsigned char* secret_out);
 
-/* ============================================================================
- * DEBUG FUNCTIONS
- * ============================================================================ */
-
 void orca_ecdh_debug_print_keypair(const OrcaECDHKeypair* keypair,
                                    const char* label);
 void orca_ecdh_debug_print_public_key(const unsigned char* public_key,
                                       const char* label);
 void orca_ecdh_debug_print_shared_secret(const unsigned char* shared_secret,
                                          const char* label);
-
-/* ============================================================================
- * TEST FUNCTIONS
- * ============================================================================ */
 
 int orca_ecdh_test_self(void);
 int orca_ecdh_test_vector(void);
@@ -150,4 +110,4 @@ int orca_ecdh_test_vector(void);
 }
 #endif
 
-#endif /* ORCA_ECDH_H */
+#endif
