6367337f30ba2c1efafb70c5e3fc654a3c436971
wWyo_O <zingert03@gmail.com>
2026-08-16 09:31:47 +0700

    Update discovery.h

--- discovery.h
+++ discovery.h
@@ -1,5 +1,4 @@
-  
-#ifndef DISCOVERY_H
+ #ifndef DISCOVERY_H
 #define DISCOVERY_H
 
 #include <stdio.h>
@@ -18,6 +17,7 @@
 
 #include "request.h"
 #include "registry.h"
+#include "orca_identity.h"
 
 #define DISCOVERY_PORT 9001
 #define MAX_PEERS 256
@@ -31,13 +31,20 @@ typedef struct {
     char name[128];
     time_t last_seen;
     bool online;
-    char public_key[256];
+    char public_key[ORCA_PUBKEY_LEN];
+    char signature[ORCA_SIG_LEN];
+    bool is_secure;
+    bool verified;
 } PeerInfo;
 
 typedef struct {
     char from_id[64];
     char from_ip[INET_ADDRSTRLEN];
     int from_port;
+    char from_name[128];
+    char public_key[ORCA_PUBKEY_LEN];
+    char signature[ORCA_SIG_LEN];
+    bool is_secure;
 } PendingRequest;
 
 typedef struct {
@@ -60,24 +67,39 @@ void discovery_destroy(Discovery* disc);
 bool discovery_init(Discovery* disc, int port);
 void discovery_start(Discovery* disc);
 void discovery_stop(Discovery* disc);
+
+// ===== Broadcasting =====
 void discovery_broadcast_presence(Discovery* disc, const char* id, const char* endpoint);
 void discovery_broadcast_search(Discovery* disc, const char* id);
 void discovery_query_peer(Discovery* disc, const char* id);
-void discovery_send_add_request_with_ack(Discovery* disc, const char* target_id, const char* my_id, const char* my_ip, int my_port);
+
+// ===== Secure Messages =====
+void discovery_send_add_request_with_ack(Discovery* disc, const char* target_id, 
+                                         const char* my_id, const char* my_ip, int my_port);
 void discovery_send_add_request_ack(Discovery* disc, const char* target_id, const char* from_id);
+
+// ===== Peer Discovery =====
 bool discovery_find_peer(Discovery* disc, const char* id, PeerInfo* out_peer);
 int discovery_get_peers(Discovery* disc, PeerInfo* peers, int max_peers);
 void discovery_cleanup_stale(Discovery* disc);
+
+// ===== Identity =====
 void discovery_set_my_identity(Discovery* disc, const char* id, const char* ip, int port);
+void discovery_set_my_secure_identity(Discovery* disc, const OrcaIdentity* identity);
+
+// ===== Callbacks =====
 void discovery_set_request_manager(RequestManager* rm);
 void discovery_set_registry(Registry* reg);
+void discovery_set_on_peer_found(Discovery* disc, void (*callback)(PeerInfo*));
+void discovery_set_on_peer_offline(Discovery* disc, void (*callback)(PeerInfo*));
+
+// ===== Pending Requests =====
 void discovery_push_pending(Discovery* disc, const char* from_id, const char* from_ip, int from_port);
 bool discovery_pop_pending(Discovery* disc, PendingRequest* out);
 int discovery_pending_count(Discovery* disc);
 bool discovery_has_pending(Discovery* disc);
+
+// ===== Utility =====
 char* discovery_get_local_ip(void);
-void discovery_set_on_peer_found(Discovery* disc, void (*callback)(PeerInfo*));
-void discovery_set_on_peer_offline(Discovery* disc, void (*callback)(PeerInfo*));
 
 #endif
- 
