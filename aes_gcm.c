3d9f57a4393a380239162738cd7ce0202a6eef37
wWyo_O <zingert03@gmail.com>
2026-08-16 13:57:45 +0700

    Update aes_gcm.c

--- aes_gcm.c
+++ aes_gcm.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <arpa/inet.h>
 
 /* ============================================================================
  * STATIC HELPERS
