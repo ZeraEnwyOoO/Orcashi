85956864ca15085e8ef12cbf7d2bd5f871e8c47e
wWyo_O <zingert03@gmail.com>
2026-08-20 08:57:46 +0700

    Update peer_list.h

--- peer_list.h
+++ peer_list.h
@@ -1,28 +1,53 @@
-// peer_list.h - Persistent accepted-peer list management
-#ifndef PEER_LIST_H
+ #ifndef PEER_LIST_H
 #define PEER_LIST_H
 
 #include <stdbool.h>
 #include <time.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
-#define MAX_PEER_LIST 1024
-
-/* Reuse RegistryPeer from registry.h */
 #include "registry.h"
 
-typedef RegistryPeer PeerListEntry;
+#define PEER_LIST_MAX_PEERS 1024
+#define PEER_LIST_MAX_ID_LEN 64
+#define PEER_LIST_MAX_IP_LEN INET_ADDRSTRLEN
+#define PEER_LIST_MAX_PORT_LEN 16
+#define PEER_LIST_MAX_STATUS_LEN 16
+#define PEER_LIST_MAX_NAME_LEN 128
+#define PEER_LIST_MAX_PUBKEY_LEN 4096
+#define PEER_LIST_MAX_SIG_LEN 512
 
 /* ============================================================================
- * Peer List - Persistent storage for accepted peers
+ * Peer List Entry (Persistent Storage)
  * ============================================================================ */
 
 typedef struct {
-    PeerListEntry peers[MAX_PEER_LIST];
+    char id[PEER_LIST_MAX_ID_LEN];
+    char ip[PEER_LIST_MAX_IP_LEN];
+    char port[PEER_LIST_MAX_PORT_LEN];
+    bool online;
+    char status[PEER_LIST_MAX_STATUS_LEN];
+    time_t last_seen;
+    RegistryMode mode;
+    char name[PEER_LIST_MAX_NAME_LEN];
+    char public_key[PEER_LIST_MAX_PUBKEY_LEN];
+    char signature[PEER_LIST_MAX_SIG_LEN];
+    char salt_hex[33];
+    time_t created_at;
+    bool verified;
+} PeerListEntry;
+
+/* ============================================================================
+ * Peer List (Persistent Storage Manager)
+ * ============================================================================ */
+
+typedef struct {
+    PeerListEntry peers[PEER_LIST_MAX_PEERS];
     int count;
     char file_path[512];
     bool dirty;
+    pthread_mutex_t mutex;
 } PeerList;
 
 /* ============================================================================
@@ -52,9 +77,24 @@ PeerListEntry* peer_list_find(PeerList* pl, const char* id);
  * ============================================================================ */
 
 int peer_list_add(PeerList* pl, const RegistryPeer* peer);
+int peer_list_add_entry(PeerList* pl, const PeerListEntry* entry);
+int peer_list_update(PeerList* pl, const char* id, const RegistryPeer* peer);
 int peer_list_remove(PeerList* pl, const char* id);
 void peer_list_clear(PeerList* pl);
+
+/* ============================================================================
+ * Sync with Registry
+ * ============================================================================ */
+
+int peer_list_sync_to_registry(PeerList* pl, Registry* reg);
+int peer_list_sync_from_registry(PeerList* pl, Registry* reg);
+
+/* ============================================================================
+ * Utility
+ * ============================================================================ */
+
 void peer_list_mark_dirty(PeerList* pl);
 bool peer_list_is_dirty(PeerList* pl);
+bool peer_list_normalize_id(const char* input, char* output, size_t output_size);
 
-#endif
+#endif /* PEER_LIST_H */
